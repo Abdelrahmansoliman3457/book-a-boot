99392e4d91658a264193c118d80a98329aa5b76a
Abdelrahmansoliman3457 <solimanabdelrahman589@gmail.com>
2023-03-30 14:59:18 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -7,26 +7,56 @@ $(window).on("load", function () {
   }, 0);
 });
 
+// toggle sidebar
+let bars = document.querySelector(".bars");
+let sideBar = document.querySelector(".links");
+
+if (bars) {
+  bars.addEventListener("click", function () {
+    this.classList.toggle("bar-tog");
+    sideBar.classList.toggle("slideto");
+  });
+}
+
+//  add active class to cuurent link
+$(".links li a").each(function () {
+  console.log(window.location.href);
+  if (window.location.href.includes($(this).attr("href"))) {
+    $(this).addClass("active");
+  }
+});
+
 // toggle drop-down
 let myBtndrop = document.querySelector(".flex-profile-d");
 let drop = document.querySelector(".my-drop-now");
-myBtndrop.addEventListener("click", function (e) {
-  drop.classList.toggle("show-drop");
-  e.stopPropagation();
-});
-document.body.addEventListener("click", function () {
-  drop.classList.remove("show-drop");
-});
-drop.addEventListener("click", function (e) {
-  e.stopPropagation();
-});
+
+if(drop){
+
+  myBtndrop.addEventListener("click", function (e) {
+    drop.classList.toggle("show-drop");
+    e.stopPropagation();
+  });
+  document.body.addEventListener("click", function () {
+    drop.classList.remove("show-drop");
+  });
+  drop.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+
+}
+
 
 // add bg color to header
 let header = document.querySelector(".header");
-window.onscroll = function () {
-  if (window.pageYOffset > 30) {
-    header.classList.add("bg-header");
-  } else {
-    header.classList.remove("bg-header");
-  }
-};
+
+if(header){
+  window.onscroll = function () {
+    if (window.pageYOffset > 30) {
+      header.classList.add("bg-header");
+    } else {
+      header.classList.remove("bg-header");
+    }
+  };
+
+}
+
